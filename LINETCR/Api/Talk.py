c698ca6c15c72a90d6493d75f9e564764c9fe165
iamwijaya <31705906+iamwijaya@users.noreply.github.com>
2018-01-16 02:50:05 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -20,8 +20,8 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/7.18.0"
-  LA = "CHROMEOS\t.1.4.5\tChrome_HP\t1"
+  UA = "FERIPAD/6.9.6.9."
+  LA = "CHROMEOS\t.1.7.14\tChrome_HP\t1"
 
   authToken = None
   cert = None
